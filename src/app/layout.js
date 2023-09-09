48c3723cb19aa602df7a35a8e3fab5d22887bbd6
Izhan-Mughal <75448107+Izhan-Mughal@users.noreply.github.com>
2023-09-10 01:35:31 +0500

    hello33

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,27 +10,28 @@ import './globals.css'
 import './admin.css'
 import Script from 'next/script'
 const inter = Inter({
-    subsets: ['latin']
-  })
+  subsets: ['latin']
+})
 
 
-  <
-  Script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-integrity = "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-strategy = "lazyOnload" / >
-  export default function RootLayout({
-    children
-  }) {
+export default function RootLayout({
+  children
+}) {
 
-    return ( <
-      html lang = "en" >
-      <
-      body className = {
-        inter.className
-      } > {
-        children
-      } < /body>  < /
-      html >
+  return ( <
+    html lang = "en" >
+    <
+    body className = {
+      inter.className
+    } > {
+      children
+    } < /body>   <
+    Script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
 
-    )
-  }
+    strategy = "lazyOnload" / >
+    <
+    /
+    html >
+
+  )
+}
