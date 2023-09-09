c66be574c3054266a2701df05b4539b16162c02b
Izhan-Mughal <75448107+Izhan-Mughal@users.noreply.github.com>
2023-09-10 01:28:50 +0500

    hello1

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,12 +21,10 @@ export default function RootLayout({
 
   return ( <
     html lang = "en" >
-    <
-    body className = {
-      inter.className
-    } > {
+    <body className = {inter.className}> {
       children
-    } < /body> < /
-    html >
+    } </body> 
+    </html>
+    
   )
 }
